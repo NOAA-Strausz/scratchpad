725bf640e72c1a24d082d7327c755c94cbac5398
David Strausz <david.strausz@noaa.gov>
2019-02-08 16:35:28 -0800

    Add parsing for atrh and baro

--- eng_wx_parse/eng_wx_parse.py
+++ eng_wx_parse/eng_wx_parse.py
@@ -12,19 +12,33 @@ file = open(filename).read()
 
 test = file.split('%%')
 wind=[]
+atrh=[]
+baro=[]
 for x in test:
     lines = x.split('\n')
     if lines[0] == 'WIND':
         if wind == []:
-
             wind=wind+lines[1:-1]
         else:
             wind=wind+lines[2:-1]
-
+    if lines[0] == 'ATRH':
+        if atrh == []:
+            atrh = atrh + lines[1:-1]
+        else:
+            atrh = atrh + lines[2:-1]
+    if lines[0] == 'BARO':
+        if baro == []:
+            baro = baro + lines[1:-1]
+        else:
+            baro = baro + lines[2:-1]
 
 for x in wind:
     print(x)
 
+for x in baro:
+    print(x)
 
+for x in atrh:
+    print(x)
             
     
